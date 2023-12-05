b56c3f12d3517b4085c446792d7aaf2765272083
Koray Kalmaz <kalmaz@gmail.com>
2023-12-05 14:14:13 +1000

    setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+setup(
+    name='ogialibs',
+    version="0.0.1",
+    url='https://github.com/Office-of-Groundwater-Impact-Assessment/ogialibs/',
+    description=(
+        "Common Python methods for OGIA"
+        ),
+    long_description=open('README.md').read(),
+    packages=find_packages(exclude=['sandbox*', 'tests*']),
+    include_package_data=True,
+    install_requires=[
+        'geopandas>=0.13',
+        'pyarrow'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Other/Nonlisted Topic'],
+)
