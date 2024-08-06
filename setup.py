564efba3910d79f3b098a3b7fb117abfd5127afb
Koray Kalmaz <kalmaz@gmail.com>
2024-08-06 12:54:12 +1000

    Removing versioneer
    removing versioneer
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py
    
    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python
-from setuptools import find_packages, setup
-import versioneer
+from setuptools import setup
 
 
 setup(
     name='ogialibs',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    url='https://github.com/Office-of-Groundwater-Impact-Assessment/ogialibs/',
+    version='0.1.1',
     description=(
         "Common Python methods for OGIA"
         ),
     long_description='README.md',
-    packages=find_packages(exclude=['sandbox*', 'tests*']),
-    include_package_data=True,
+    packages=["ogialibs",],
     install_requires=[
         'geopandas>=0.13',
         'pyarrow',
     ],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Other/Nonlisted Topic'],
 )
